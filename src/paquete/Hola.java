f21e9440665ee5165f85617404258fe0363eeb0d
mi <mi@DESKTOP-AL32H70>
2023-02-10 19:49:14 +0100

    subir

---
+++ src/paquete/Hola.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Hola {
+	
+	// Tu programa comienza con una llamada a main().
+    // Imprime "Hola Mundo" a la ventana de la terminal.
+    public static void main(String args[])
+    {
+        System.out.println("Hola Mundo");
+        //añade aqui otro System.out.println con tu nombre completo
+    }
+	
+
+}
